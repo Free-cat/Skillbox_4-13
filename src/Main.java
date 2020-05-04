8da03ce69e29ea5d9a99707907f993cfef83043d
Артем Мельников <a.melnikov@carex.su>
2020-05-04 23:10:55 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Byte MIN: " + Byte.MIN_VALUE + " MAX: " + Byte.MAX_VALUE);
+        System.out.println("Short MIN: " + Short.MIN_VALUE + " MAX: " + Short.MAX_VALUE);
+        System.out.println("Integer MIN: " + Integer.MIN_VALUE + " MAX: " + Integer.MAX_VALUE);
+        System.out.println("Long MIN: " + Long.MIN_VALUE + " MAX: " + Long.MAX_VALUE);
+        System.out.println("Float MIN: " + Float.MIN_VALUE + " MAX: " + Float.MAX_VALUE);
+        System.out.println("Double MIN: " + Double.MIN_VALUE + " MAX: " + Double.MAX_VALUE);
+    }
+}
